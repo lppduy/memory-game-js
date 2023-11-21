fbd259082142bcc2ce3599fabdadf789c6c93862
lppduy <lephamphuongduy@gmail.com>
2023-11-21 21:00:43 +0700

    change variable name of class Sprite to be more general

--- engine/Sprite.js
+++ engine/Sprite.js
@@ -1,9 +1,9 @@
 import { Node } from './Node.js';
 
 export class Sprite extends Node {
-  constructor(nameCard) {
+  constructor(imageSoure) {
     super();
-    this._src = this._getSrc(nameCard);
+    this._src = this._getSrc(imageSoure);
     this.element = document.createElement('img');
     this.element.src = this._src;
     this.element.classList.add('card');
@@ -15,7 +15,7 @@ export class Sprite extends Node {
   set src(value) {
     return (this._src = value);
   }
-  _getSrc(nameCard) {
-    return `./assets/${nameCard.toString()}.jpg`;
+  _getSrc(imageSoure) {
+    return `./assets/${imageSoure.toString()}.jpg`;
   }
 }
